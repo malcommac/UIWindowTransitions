6335b235446f49c05c22faf871600ad6e05b1032
Jesse Gatt <jesse.gatt@organizer.com>
2019-06-07 14:03:59 -0700

    upgrade project to swift 5, bump version to 0.3.0

--- Sources/UIWindowTransitions/UIWindowTransitions.swift
+++ Sources/UIWindowTransitions/UIWindowTransitions.swift
@@ -12,7 +12,7 @@ import UIKit
 public extension UIWindow {
 	
 	/// Transition Options
-	public struct TransitionOptions {
+	struct TransitionOptions {
 		
 		/// Curve of animation
 		///
@@ -125,7 +125,7 @@ public extension UIWindow {
 	/// - Parameters:
 	///   - controller: controller to set
 	///   - options: options of the transition
-	public func setRootViewController(_ controller: UIViewController, options: TransitionOptions = TransitionOptions()) {
+	func setRootViewController(_ controller: UIViewController, options: TransitionOptions = TransitionOptions()) {
 		
 		var transitionWnd: UIWindow? = nil
 		if let background = options.background {
